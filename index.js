abf71f87cc2dea8a1fe752bcfc9b104d2703af52
AdamCampeau <adamcampeau3@gmail.com>
2022-02-12 09:50:25 -0500

    finished view all

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function startingQuestion() {
             type: 'list',
             name: 'startingQuestion',
             message: 'What would you like to do?',
-            choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employees Role', 'Done']
+            choices: ['View Departments', 'View Roles', 'View Employees', 'View All', 'Add Department', 'Add Role', 'Add Employee', 'Update Employees Role', 'Done']
         }
     ]).then(answer => {
         if (answer.startingQuestion === 'View Departments') {
@@ -32,6 +32,9 @@ function startingQuestion() {
         }
         else if (answer.startingQuestion === 'View Employees') {
             viewEmployees()
+        }
+        else if (answer.startingQuestion === 'View All') {
+            viewAll()
         }
         else if (answer.startingQuestion === 'Add Department') {
             addDepartment()
@@ -133,6 +136,16 @@ function viewRoles() {
 
 function viewEmployees() {
     connection.query('SELECT * FROM employee;', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        //viewEmployees();
+        startingQuestion()
+    })
+}
+
+// VIEW ALL
+function viewAll() {
+    connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", (err, res) => {
         if (err) throw err;
         console.table(res);
         //viewEmployees();
